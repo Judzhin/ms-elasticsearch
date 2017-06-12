efd97b675984ff91084fed84afe41e1dca35df20
Judzhin Miles <info@msbios.com>
2017-06-12 13:22:17 +0300

    Intermedaite commit

--- bin/getting-started.php
+++ bin/getting-started.php
@@ -13,7 +13,7 @@ $client = \Elasticsearch\ClientBuilder::create()
 $params = [
     'index' => 'some_index',
     'type' => 'some_type',
-    'id' => 'some_id 1',
+    'id' => 'some_id',
     'body' => [
         'Field 1' => 'Value 1',
         'Field 2' => 'Value 2',
@@ -22,5 +22,25 @@ $params = [
 ];
 
 /** @var array $response */
-$response = $client->index($params);
-print_r($response);
+$response = $client->index($params); // Update or Inser if not exist
+/**
+ * Array(
+ *     [_index] => some_index
+ *     [_type] => some_type
+ *     [_id] => some_id 1
+ *     [_version] => 3
+ *     [_shards] => Array(
+ *                      [total] => 2
+ *                      [successful] => 1
+ *                      [failed] => 0
+ *                  )
+ *     [created] =>
+ * )
+ * <code>
+ *     print_r($response);
+ * </code>
+ */
+
+unset ($params['body']);
+/** @var array $response */
+$response = $client->delete($params); // Delete
